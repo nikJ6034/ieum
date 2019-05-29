0a9b5f468ba2a076b6c557b8a9d9750dea457994
nikJ <jmi6034@gmail>
2019-05-29 15:41:50 +0900

    마을학교 지도 넣기

--- front/eum/src/page/Directions.vue
+++ front/eum/src/page/Directions.vue
@@ -18,9 +18,9 @@
                 <h4 class="tit-01">안녕하세요. 이음 마을학교를 찾아주셔서 짐심으로 감사합니다.</h4>
 
                 <strong class="tit-i-01">약도</strong>
-                이미지
+                <div id="map" style="width:100%;height:400px;border: solid 2px #ffad55;"></div>
+                <br/>
                 <strong class="tit-03">이음마을학교</strong>
-
                 <ul class="location-list">
                     <li>
                         <span>주소</span>
@@ -48,7 +48,35 @@ import SubLayout from '@/components/layouts/SubLayout'
     name: 'directions',
     props: [],
     mounted() {
+      var container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
+      var options = { //지도를 생성할 때 필요한 기본 옵션
+        center: new daum.maps.LatLng(36.365632, 127.431161), //지도의 중심좌표.
+        level: 3 //지도의 레벨(확대, 축소 정도)
+      };
 
+      var map = new daum.maps.Map(container, options); //지도 생성 및 객체 리턴
+
+      // 마커가 표시될 위치입니다 
+      var markerPosition  = new daum.maps.LatLng(36.365660, 127.431150); 
+
+      // 마커를 생성합니다
+      var marker = new daum.maps.Marker({
+          position: markerPosition
+      });
+
+      // 마커가 지도 위에 표시되도록 설정합니다
+      marker.setMap(map);
+
+      var iwContent = '<div style="padding:5px 29px">이음 마을학교</div>', // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
+      iwPosition = new daum.maps.LatLng(36.365632, 127.431161); //인포윈도우 표시 위치입니다
+
+      // 인포윈도우를 생성합니다
+      var infowindow = new daum.maps.InfoWindow({
+          position : iwPosition, 
+          content : iwContent 
+      });
+
+      infowindow.open(map, marker); 
     },
     data() {
       return {
